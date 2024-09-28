18c731dcf13fecd147b9b4a8ddfc2dfe1856f473
Villadsg <gvjorholt@gmail.com>
2024-09-28 20:31:12 +0200

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -35,7 +35,7 @@
   </div>
   <div class="flex-none">
     <div class="dropdown dropdown-end">
-      <button class="btn btn-ghost btn-circle" aria-label="Menu">
+      <button class="btn btn-ghost btn-circle text-white" aria-label="Menu">
         <!-- Hamburger Icon -->
         <svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
           <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
