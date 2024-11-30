d7c33dbd33424160a44faffa4df7cbe6d1a905e4
Villadsg <gvjorholt@gmail.com>
2024-11-30 15:16:34 -0600

    Update +page.svelte

--- src/routes/find-quiz/+page.svelte
+++ src/routes/find-quiz/+page.svelte
@@ -158,7 +158,7 @@ const closeTooFarMessage = () => {
               <img
                 src="{monument.photoUrl}"
                 alt="{monument.name}"
-                class="max-w-xs rounded shadow mx-auto"
+                class="my-3 rounded shadow "
               />
             {/if}
 
